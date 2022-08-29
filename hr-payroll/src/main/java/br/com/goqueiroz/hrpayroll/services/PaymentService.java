3007e9523ee575c31872e8b4abf58ffe3c4e3b9f
thiagorocha <thiago.rocha@soulisto.com.br>
2022-08-29 19:12:56 -0300

    rest template

--- hr-payroll/src/main/java/br/com/goqueiroz/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/goqueiroz/hrpayroll/services/PaymentService.java
@@ -1,12 +1,34 @@
 package br.com.goqueiroz.hrpayroll.services;
 
+import br.com.goqueiroz.hrpayroll.config.AppConfig;
 import br.com.goqueiroz.hrpayroll.entities.Payment;
+import br.com.goqueiroz.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+  private RestTemplate restTemplate;
+  private AppConfig config;
+
+
+  @Autowired
+  public PaymentService(RestTemplate restTemplate, AppConfig config) {
+    this.restTemplate = restTemplate;
+    this.config = config;
+  }
+
   public Payment getPayment(Long workerId, int days){
-    return new Payment("Bob", 200.0, days);
+    Map<String, String> uriVariable = new HashMap<>();
+    uriVariable.put("id", workerId.toString());
+
+    Worker worker = restTemplate.getForObject(config.getWorkerHost() + "/workers/{id}", Worker.class, uriVariable);
+
+    return new Payment(worker.getName(), worker.getDailyIncome(), days);
   }
 }
