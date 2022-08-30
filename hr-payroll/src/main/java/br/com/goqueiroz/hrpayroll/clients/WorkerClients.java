88790626ad8f43983166ed3f6834c2abfdf76063
thiagorocha <thiago.rocha@soulisto.com.br>
2022-08-30 10:56:46 -0300

    add open feign

---
+++ hr-payroll/src/main/java/br/com/goqueiroz/hrpayroll/clients/WorkerClients.java
@@ -0,0 +1,16 @@
+package br.com.goqueiroz.hrpayroll.clients;
+
+import br.com.goqueiroz.hrpayroll.entities.Worker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+public interface WorkerClients {
+
+    @GetMapping(value = "/{id}")
+    ResponseEntity<Worker> findById(@PathVariable Long id);
+}
--- hr-payroll/src/main/java/br/com/goqueiroz/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/goqueiroz/hrpayroll/services/PaymentService.java
@@ -1,9 +1,11 @@
 package br.com.goqueiroz.hrpayroll.services;
 
+import br.com.goqueiroz.hrpayroll.clients.WorkerClients;
 import br.com.goqueiroz.hrpayroll.config.AppConfig;
 import br.com.goqueiroz.hrpayroll.entities.Payment;
 import br.com.goqueiroz.hrpayroll.entities.Worker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,22 +14,15 @@ import java.util.Map;
 
 @Service
 public class PaymentService {
-
-  private RestTemplate restTemplate;
-  private AppConfig config;
-
+  private WorkerClients workerClients;
 
   @Autowired
-  public PaymentService(RestTemplate restTemplate, AppConfig config) {
-    this.restTemplate = restTemplate;
-    this.config = config;
+  public PaymentService(WorkerClients workerClients) {
+    this.workerClients = workerClients;
   }
 
   public Payment getPayment(Long workerId, int days){
-    Map<String, String> uriVariable = new HashMap<>();
-    uriVariable.put("id", workerId.toString());
-
-    Worker worker = restTemplate.getForObject(config.getWorkerHost() + "/workers/{id}", Worker.class, uriVariable);
+    Worker worker = workerClients.findById(workerId).getBody();
 
     return new Payment(worker.getName(), worker.getDailyIncome(), days);
   }
