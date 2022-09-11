5e54b285adb86f68259aff42ce7a40eff910213a
Gabriel Queiroz <gabriel-cso@hotmail.com>
2022-09-10 23:30:14 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/br/com/goqueiroz/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/br/com/goqueiroz/hrworker/controller/WorkerController.java
@@ -2,7 +2,10 @@ package br.com.goqueiroz.hrworker.controller;
 
 import br.com.goqueiroz.hrworker.entities.Worker;
 import br.com.goqueiroz.hrworker.services.WorkerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,10 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 public class WorkerController {
 
+  private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+
+  @Autowired
+  public Environment env;
   private WorkerService workerService;
 
   @Autowired
@@ -31,6 +38,8 @@ public class WorkerController {
 
   @GetMapping(value = "/{id}")
   public ResponseEntity<Worker> findById(@PathVariable Long id){
+
+    logger.info("port = " + env.getProperty("local.server.port"));
 
     Worker worker = workerService.findById(id);
 
