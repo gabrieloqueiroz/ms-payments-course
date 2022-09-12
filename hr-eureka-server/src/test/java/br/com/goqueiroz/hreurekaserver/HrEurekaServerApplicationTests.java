e496b27b0a019e787532d33c9f4161db37453e18
Gabriel Queiroz <gabriel-cso@hotmail.com>
2022-09-12 18:48:38 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/br/com/goqueiroz/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.goqueiroz.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
