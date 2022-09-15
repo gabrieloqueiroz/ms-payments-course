77c4f9b84d51dd39c53c09b64c72a2137b7d943b
Gabriel Queiroz <gabriel-cso@hotmail.com>
2022-09-14 21:36:58 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/br/com/goqueiroz/hrpayroll/controller/PaymentController.java
+++ hr-payroll/src/main/java/br/com/goqueiroz/hrpayroll/controller/PaymentController.java
@@ -2,6 +2,7 @@ package br.com.goqueiroz.hrpayroll.controller;
 
 import br.com.goqueiroz.hrpayroll.entities.Payment;
 import br.com.goqueiroz.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,16 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/payments")
 public class PaymentController {
 
-  private PaymentService paymentService;
+    private PaymentService paymentService;
 
-  @Autowired
-  public PaymentController(PaymentService paymentService){
-    this.paymentService = paymentService;
-  }
+    @Autowired
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
 
-  @GetMapping(value = "/{workerId}/days/{days}")
-  public ResponseEntity<Payment> getTotal(@PathVariable Long workerId, @PathVariable Integer days){
-    Payment responsePayment = paymentService.getPayment(workerId, days);
-    return ResponseEntity.ok(responsePayment);
-  }
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
+    @GetMapping(value = "/{workerId}/days/{days}")
+    public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
+        Payment responsePayment = paymentService.getPayment(workerId, days);
+        return ResponseEntity.ok(responsePayment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(@PathVariable Long workerId, @PathVariable Integer days) {
+        Payment payment = new Payment("Fail", 0.00, 0);
+        return ResponseEntity.ok(payment);
+    }
 }
